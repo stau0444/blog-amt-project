f2848c52f34a9e1a5708184d1f6f658c0677214d
stau04 <stau04@gmail.com>
2021-03-12 13:36:50 +0900

    vue.config.js  수정

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer:{
         proxy:{
             '/api':{
-                target:'http://localhost:8090/',
+                target:'http://localhost:8080/',
                 ws:true,
                 changeOrigin:true
             },
